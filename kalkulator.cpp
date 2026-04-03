572b9aefc3c59425192afaf492d8b886885e38cb
daffa <daffabudiono76@gmail.com>
2026-04-03 08:51:35 +0700

    kalkulator
    perhungan

---
+++ kalkulator.cpp
@@ -0,0 +1,12 @@
+#inclaude <iostream>
+using namespace std;
+
+int main() {
+int a, b;
+cout <<"masukkan angka pertma":;
+cin >>a;
+cout << "masukkan angka kedua: ";
+cin >>b;
+cout << "Hasi:l " << a + b << endl;
+return 0;
+}
